dfe66da86b28c16d575989b20669ea01a7106979
Rebekah Heal <heal.rebekah@gmail.com>
2020-07-13 09:21:48 +1000

    created database option, read files saved in input

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+# os.system('clear')
 
 
 def gather_ingredients():
@@ -11,6 +13,7 @@ def gather_ingredients():
             continue
         amount = input(f"Amount of {ingredient}: ")
         ingredients[ingredient] = amount
+    return ingredients
 
 def gather_recipe_steps():
     count = 1
@@ -39,7 +42,7 @@ def save_recipe(recipe_name, compiled_recipe):
     text_file.close()
 
 
-
+os.system('clear')
 
 print("""Welcome to {}!
 This is an app that allows you to create recipes and store them in your own personal database.
@@ -54,8 +57,12 @@ When you're done looking at your recipes you can always exit the application ent
 choice = input("What would you like to do? ")
 
 if choice == 'input':
+    os.system('clear')
+
     print("I see you've decided to input a recipe!")
-    recipe_name = input("What is the name of the recipe? ")
+    recipe_name = input("What is the name of the recipe? ").title()
+
+    os.system('clear')
 
     print("Sounds delicious!")
     print("Now let's enter some ingredients. We'll enter the ingredients first and then the amount that we need after.")
@@ -63,10 +70,14 @@ if choice == 'input':
 
     ingredients_list = gather_ingredients()
 
+    os.system('clear')
+
     print("Now that we've entered the ingredients, let's type in the method.")
     print("Once again you can type in 'done' when you are finished.")
 
     recipe_steps = gather_recipe_steps()
+
+    os.system('clear')
 
     print("Compiling recipe . . .")
 
@@ -74,32 +85,49 @@ if choice == 'input':
 
     print(compiled_recipe)
 
-    save = input("Would you like to save the recipe to your database? (y/n) ")
+    save = input("\nWould you like to save the recipe to your database? (y/n) ")
 
     if save == 'y':
-        save_recipe(recipe_name, compiled_recipe)
+        save_recipe(os.path.join('./recipe_database/', recipe_name.lower()), compiled_recipe)
         print("Your recipe has been saved.")
+
     else:
         print("Deleting data.")
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 elif choice == 'database':
-    pass
+    os.system('clear')
+    print("Accessing data . . . ")
+
+    database_list = []
+
+    for root, directories, files in os.walk('./recipe_database/'):
+        for file in files:
+            database_list.append(os.path.join('./recipe_database/', file))
+
+    for i in range(0, len(database_list)):
+        database_list[i] = database_list[i].replace("./recipe_database/", "").replace(".txt", "").lower()
+        print(database_list[i].title())
+
+    access = input("\nWould you like to access a recipe? (y/n) ")
+    if access == 'y':
+        recipe_to_access = input("Which recipe would you like to access? ")
+        if recipe_to_access.lower() in database_list:
+            file_to_open = open(os.path.join('./recipe_database/', database_list[database_list.index(recipe_to_access)] + '.txt'), 'r')
+            for line in file_to_open:
+                print(line, end="")
+            print("\n")
+
+
+    elif access == 'n':
+        input("Type 'main' to return to the main menu. ")
+    else:
+        print("Command not understood.")
+        input("Enter a valid command. ")
+
+
 elif choice == 'help':
     pass
 else:
